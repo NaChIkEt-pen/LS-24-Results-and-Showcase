39eaf1526a11408c3330628b1bf2ccf1f1ec394e
Nachiket Pensalwar <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-06-17 13:06:26 +0530

    updates: style to elements add : config.toml

--- index.py
+++ index.py
@@ -2,52 +2,84 @@ import folium
 import json
 import streamlit as st
 from streamlit_folium import st_folium
+st.set_page_config(layout="wide")
 
+st.markdown("""
+        <style>
+        .block-container {
+            padding-top: 4rem;
+            padding-bottom: 0rem;
+            padding-left: 0rem;
+            padding-right: 0rem;
+        }
+        .main {
+            padding-left: 2rem;
+            padding-right: 4rem;
+        }
+        #map-container {
+            margin-bottom: 0px;
+        }
+    </style>
+        """, unsafe_allow_html=True)
+
+
+
+st.markdown('<div id="title-div">', unsafe_allow_html=True)
 st.title("Lok Sabha 2024 Elections")
 st.caption("Project showcases the general data and stats of the election in an interactive way.")
-col1, col2 = st.columns(3)
+st.markdown('</div>', unsafe_allow_html=True)
+
+
+col1, col2 = st.columns([1, 0.5],gap="large")
 
 ######### Map ###############
-# Define the path to your local GeoJSON file
-geojson_file_path = 'india_pc_2019.geojson'
+with col1:
+    # Define the path to your local GeoJSON file
+    geojson_file_path = 'india_pc_2019.geojson'
 
-# Read the GeoJSON data from the file with specified encoding
-with open(geojson_file_path, 'r', encoding='utf-8') as geojson_file:
-    geojson_data = json.load(geojson_file)
+    # Read the GeoJSON data from the file with specified encoding
+    with open(geojson_file_path, 'r', encoding='utf-8') as geojson_file:
+        geojson_data = json.load(geojson_file)
 
-# Create a map object centered at a particular latitude and longitude
-# Adjust the location and zoom level as needed
-m = folium.Map(location=[23.5, 83.0882],
-        zoom_start=4.5,
-        tiles='',
-        attr="India only"
-        )
+    # Create a map object centered at a particular latitude and longitude
+    # Adjust the location and zoom level as needed
+    m = folium.Map(location=[22.5, 85.0882],
+            zoom_start=4,
+            tiles='',
+            attr="India only"
+            )
 
-# style_function = lambda x: {'fillColor': '#ffffff', 
-#                             'color':'#000000', 
-#                             'fillOpacity': 0.1, 
-#                             'weight': 0.1}
+    # style_function = lambda x: {'fillColor': '#ffffff', 
+    #                             'color':'#000000', 
+    #                             'fillOpacity': 0.1, 
+    #                             'weight': 0.1}
 
-# Add the GeoJSON layer to the map  #########  , style_function=style_function
-g = folium.GeoJson(geojson_data, name="geojson" ).add_to(m)
-m.add_child(folium.LatLngPopup())
+    # Add the GeoJSON layer to the map  #########  , style_function=style_function
+    g = folium.GeoJson(geojson_data, name="geojson" ).add_to(m)
+    m.add_child(folium.LatLngPopup())
 
-# # Add layer control to toggle GeoJSON visibility
-# folium.LayerControl().add_to(m)
+    # # Add layer control to toggle GeoJSON visibility
+    # folium.LayerControl().add_to(m)
 
-# Save the map to an HTML file
-# m.save('map_with_geojson.html')
+    # Save the map to an HTML file
+    # m.save('map_with_geojson.html')
 
-folium.GeoJsonTooltip(fields=["pc_name"],style="background-color: red;").add_to(g)
-st_data  = st_folium(m, width=700)
-# If using Jupyter Notebook, display the map inline
-# m
-def get_pos(lat, lng):
-    return lat, lng
+    folium.GeoJsonTooltip(fields=["pc_name"],style="background-color: red;").add_to(g)
+    st.markdown('<div id="map-container">', unsafe_allow_html=True)
+    st_data  = st_folium(m,width=500, height=400)
+    st.markdown('</div>', unsafe_allow_html=True)
+    # If using Jupyter Notebook, display the map inline
+    # m
+    def get_pos(lat, lng):
+        return lat, lng
 
-if st_data is not None and 'last_clicked' in st_data and st_data['last_clicked'] is not None:
-    lat_long_data = get_pos(st_data['last_clicked']['lat'], st_data['last_clicked']['lng'])
-    # print(st_data)
-    print(lat_long_data)
-else:
-    st.subheader("Click on the map to get details of the Parliamentary Constituency.")
+    if st_data is not None and 'last_clicked' in st_data and st_data['last_clicked'] is not None:
+        lat_long_data = get_pos(st_data['last_clicked']['lat'], st_data['last_clicked']['lng'])
+        # print(st_data)
+        print(lat_long_data)
+    else:
+        st.caption("Click on the map to get details of the Parliamentary Constituency.")
+
+
+with col2:
+    st.subheader("State / Counstitunecy wise data")
